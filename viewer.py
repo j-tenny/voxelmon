8aa7e909b0ebe9a219382d06cea4cff8016d0aa5
Johnathan Tenny <107655580+j-tenny@users.noreply.github.com>
2025-03-05 13:36:49 -0700

    Add separate color for non-foliage

--- viewer.py
+++ viewer.py
@@ -12,7 +12,6 @@ from tkinter import ttk
 
 #grid_path = 'D:/DataWork/pypadResults/PAD/T1423070502.csv'
 grid_path = 'D:/DataWork/pypadResults/PAD/T1423081501.csv'
-#grid_path = 'D:/DataWork/SanCarlos/Pre/PAD/AZSCA_0018_2022_06301159.csv'
 #clip_extents = 'auto'
 clip_extents = [-11.3,-.5,11.3,.5]
 #clip_extents = [-.5,-11.3,.5,11.3] # [minx, miny, minz, maxx, maxy, maxz], [minx, miny, maxx, maxy] or 'auto'
@@ -24,8 +23,7 @@ points_path = Path('/'.join(grid_path.parts[:-2])+'/Points/'+basename)
 
 
 
-def visualizeVoxels(grid_path, points_path, dem_path, clip_extents = 'auto', value_name = 'pad', max_value = 6,
-                    color_map=['#386404','#386404','#386404','#386404','#386404', '#386404', '#4E3118']):
+def visualizeVoxels(grid_path, points_path, dem_path, clip_extents = 'auto', value_name = 'pad', max_value = 6):
 
     global pl,pts_fill_actor,pts_fill_actor_leaf,pts_fill_actor_wood,pts_occluded_actor, filled_values
 
@@ -63,18 +61,22 @@ def visualizeVoxels(grid_path, points_path, dem_path, clip_extents = 'auto', val
                                  demPoints[:,1].reshape(shape[0:2]),
                                  demPoints[:,2].reshape(shape[0:2])).texture_map_to_plane()
 
+    leaf_mask = (polars.col('classification')>0) & (polars.col('hag')>=.1) & (polars.col(value_name)<=max_value)
+    occluded_mask = polars.col('classification')==-1
+    wood_mask = (polars.col('classification')>0) & (polars.col('hag')>=.1) & (polars.col(value_name)>max_value)
 
-
-    filled_pts = pyvista.PolyData(grid.filter((polars.col('classification')>0) & (polars.col('hag')>=.1))[:,:3].to_numpy())
-    filled_values = grid.filter((polars.col('classification')>0) & (polars.col('hag')>=.1))[value_name].to_numpy()
+    filled_pts = pyvista.PolyData(grid.filter(leaf_mask)[:,:3].to_numpy())
+    filled_values = grid.filter(leaf_mask)[value_name].to_numpy()
     filled_values = np.repeat(filled_values,6)
-    occluded_pts = pyvista.PolyData(grid.filter(polars.col('classification')==-1)[:,:3].to_numpy())
+    occluded_pts = pyvista.PolyData(grid.filter(occluded_mask)[:,:3].to_numpy())
+    wood_pts = pyvista.PolyData(grid.filter(wood_mask)[:,:3].to_numpy())
 
     base_cube = pyvista.Cube((0,0,0),cellSize,cellSize,cellSize)
     scanner_leg = pyvista.Cylinder((0,0,-1),direction=(0,0,1),radius=.05,height=2)
     scanner_head = pyvista.Sphere(radius=.2)
 
     filled_glyphs = filled_pts.glyph(orient=False,scale=False,geom=base_cube)
+    wood_glyphs = wood_pts.glyph(orient=False,scale=False,geom=base_cube)
     occluded_glyphs = occluded_pts.glyph(orient=False,scale=False,geom=base_cube)
 
     pl = pyvista.Plotter(lighting='three lights')
@@ -86,13 +88,9 @@ def visualizeVoxels(grid_path, points_path, dem_path, clip_extents = 'auto', val
     pl.enable_terrain_style(mouse_wheel_zooms=True, shift_pans=True)
     pyvista.global_theme.full_screen = True
 
-    if value_name == 'classification':
-        filled_values = filled_values.astype(int)
-        pts_fill_actor = pl.add_mesh(filled_glyphs, scalars=filled_values, opacity=1,
-                                     show_scalar_bar=False, cmap=color_map, categories=True)
-    else:
-        pts_fill_actor = pl.add_mesh(filled_glyphs, clim=[0, max_value], scalars=filled_values, opacity=1,
-                                     show_scalar_bar=False,cmap='YlGn')
+
+    pts_fill_actor = pl.add_mesh(filled_glyphs, clim=[0, max_value], scalars=filled_values, opacity=1, show_scalar_bar=False,cmap='YlGn')
+    pts_fill_actor_wood = pl.add_mesh(wood_glyphs, color='brown', opacity=1)
     pts_occluded_actor = pl.add_mesh(occluded_glyphs,color=True,opacity=.25)
     pts_all_actor = pl.add_mesh(pts_all,scalars=points[:,2],clim=[-3, 12], show_scalar_bar=False)
     pts_all_actor.SetVisibility(False)
@@ -111,11 +109,8 @@ def visualizeVoxels(grid_path, points_path, dem_path, clip_extents = 'auto', val
         global pl
         global pts_fill_actor, pts_fill_actor_leaf, pts_fill_actor_wood,filled_values
         pl.remove_actor(pts_fill_actor)
-        if value == 'classification':
-            filled_values = filled_values.astype(int)
-            pts_fill_actor = pl.add_mesh(filled_glyphs, scalars=filled_values, opacity=value, show_scalar_bar=True, cmap=color_map, categories=True)
-        else:
-            pts_fill_actor = pl.add_mesh(filled_glyphs, clim=[0, max_value], scalars=filled_values, opacity=value,show_scalar_bar=True,cmap='YlGn')
+        pts_fill_actor = pl.add_mesh(filled_glyphs, clim=[0, max_value], scalars=filled_values, opacity=value,show_scalar_bar=True,cmap='YlGn')
+        pts_fill_actor_wood = pl.add_mesh(wood_glyphs, color='brown', opacity=value)
     pl.add_slider_widget(set_filled_opacity, [0, 1], title='Filled Voxel Opacity',pointa=(.1,.9),pointb=(.3,.9),value=1)
 
     def set_occluded_opacity(value):
